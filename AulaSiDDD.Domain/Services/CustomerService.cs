37309022442ed73b87b64020c57693a4fad862b0
Aluno <aluno@cesed.local>
2016-09-15 21:56:59 -0300

    Implementing Customer and CustomerService

---
+++ AulaSiDDD.Domain/Services/CustomerService.cs
@@ -0,0 +1,27 @@
+﻿using AulaSiDDD.Domain.Interfaces.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaSiDDD.Domain.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        public List<Customer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer GetCustomer(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetEmail(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
